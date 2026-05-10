dd30d908dc0605bf89d0270af73613d01691cecb
rpmeuro <theinboxinnovator@gmail.com>
2026-05-10 11:37:32 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
-#include<vector>
 #include<queue>
 #include<omp.h>
-
 using namespace std;
+
 
 struct Node {
         int data;
         Node *left,*right;
 };
 
-Node* insert(Node* root,int data) {
+Node *insert(Node* root,int data) {
         if(!root) {
                 root = new Node{data,NULL,NULL};
                 return root;
@@ -20,73 +19,70 @@ Node* insert(Node* root,int data) {
         q.push(root);
 
         while(!q.empty()) {
-
-                Node* temp = q.front();
+                Node *temp = q.front();
                 q.pop();
 
-                if(!temp->left){
+                if(!temp->left) {
                         temp->left = new Node{data,NULL,NULL};
                         return root;
                 } else {
                         q.push(temp->left);
                 }
 
-                if(!temp -> right) {
-                        temp -> right = new Node{data,NULL,NULL};
+                if(!temp->right) {
+                        temp->right = new Node{data,NULL,NULL};
                         return root;
                 } else {
-                        q.push(temp-> right);
+                        q.push(temp->right);
                 }
         }
         return root;
 }
 
-void parallelBFS(Node *root) {
-        if(!root) return;
+void bfs(Node *root) {
+        queue<Node*> q;
+        q.push(root);
 
-        vector<Node*> currentLevel;
-        currentLevel.push_back(root);
+        while(!q.empty()) {
+                int size = q.size();
 
-        while(!currentLevel.empty()) {
-                vector<Node*> nextLevel;
+                #pragma omp parallel for
+                for(int i=0;i<size;i++) {
+                        Node* curr;
 
-                #pragma omp parallel
-                {
-                        vector<Node*> localNext;
-
-                        #pragma omp for
-                        for(int i=0;i < currentLevel.size();i++) {
-                                Node* curr = currentLevel[i];
-
-                                #pragma omp critical
+                        #pragma omp critical
+                        {
+                                curr = q.front();
+                                q.pop();
                                 cout<<curr->data<<" ";
-
-                                if(curr->left) localNext.push_back(curr->left);
-                                if(curr->right) localNext.push_back(curr->right);
                         }
 
                         #pragma omp critical
-                        nextLevel.insert(nextLevel.end(),localNext.begin(),localNext.end());
+
+                        {
+                                if(curr->left) q.push(curr->left);
+                                if(curr->right) q.push(curr->right);
+                        }
                 }
-                currentLevel = nextLevel;
         }
 }
 
 
 int main() {
-        Node *root = NULL;
         int data;
         char ch;
+        Node *root=NULL;
 
-        do {
+        do{
                 cout<<"Enter Data : ";
                 cin>>data;
                 root = insert(root,data);
-
-                cout<<"Add more nodes (y/n)";
+                cout<<"Add more nodes : ";
                 cin>>ch;
-        } while(ch=='y' || ch == 'Y');
-        cout<<"\n Parallel BFS Traversal : \n";
-        parallelBFS(root);
+        } while(ch == 'y' || ch=='Y');
+
+        cout<<"BFS Traversal : "<<endl;
+
+        bfs(root);
         return 0;
 }
